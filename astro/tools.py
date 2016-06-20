601e183dc6fca564e030a2990a176182caa657f1
Johnny Greco <jgreco@astro.princeton.edu>
2016-06-20 14:50:40 -0400

    added arcmin return option

--- astro/tools.py
+++ astro/tools.py
@@ -6,20 +6,29 @@ def angsep(ra1, dec1, ra2, dec2, sepunits='arcsec'):
 
     Inputs
     ------
-    ra1:  float or ndarray, right ascension(s) of first point (in degrees)
-    dec1: float or ndarray, declination(s) of first point (in degrees)
-    ra2:  float or ndarray, right ascension(s) of second point (in degrees)
-    dec2: float or ndarray, declination(s) of second point (in degrees)
+    ra1 :  float or ndarray 
+        Right ascension(s) of first point(s) (in degrees). The shape
+        must be the same as dec1.
+    dec1 : float or ndarray 
+        Declination(s) of first point(s) (in degrees). The shape must
+        be the same as ra1.
+    ra2 :  float or ndarray 
+        Right ascension(s) of second(s) point (in degrees). The shape
+        must be the same as dec2.
+    dec2 : float or ndarray 
+        Declination(s) of second point(s) (in degrees). The shape must
+        be the same as ra2.
+    sepunits : string, optional, default = 'arcsec'
+        Angular unit of the returned separation
+        (radian, degree, arcsec, or arcmin)
 
-    Optional input
-    --------------
-    sepunits: string, angular unit of the returned separation
-              (radian, degree, or arcsec; default = arcsec)
+    Returns
+    -------
+    sep : same type as input angles, angular separation between points
 
-    Returns:
-    sep: same type as input angles, angular separation between points
-
-    Note: This is function uses the Vincenty formula:
+    Note
+    ----
+    This is function uses the Vincenty formula:
     https://en.wikipedia.org/wiki/Great-circle_distance.
     """
     deg2rad = np.pi/180.0
@@ -41,7 +50,7 @@ def angsep(ra1, dec1, ra2, dec2, sepunits='arcsec'):
     denom = sin_dec1*sin_dec2 + cos_dec1*cos_dec2*cos_dRA
     sep = np.arctan2(np.sqrt(num1*num1 + num2*num2), denom)
 
-    conversion = {'radian':1.0, 'arcsec':206264.806, 'degree':180./np.pi}
+    conversion = {'radian':1.0, 'arcsec':206264.806, 'arcmin':206264.806/60.0, 'degree':180./np.pi}
     sep *= conversion[sepunits]
 
     return sep
